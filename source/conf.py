9b33b71262993b106611887edcee2f19714a03ec
bakr.aassoul <sorry.no.sauce.foru@gmail.com>
2025-10-05 16:01:30 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -32,11 +32,7 @@ latex_elements = {
         \usepackage{lmodern}
         \usepackage{fancyhdr}
         \usepackage{graphicx}
-        \usepackage{fvextra}
-        \fvset{
-          breaklines=true,
-          breakanywhere=true
-        }
+    
         \makeatletter
         \fancypagestyle{normal}{
             \fancyhf{}
